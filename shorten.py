b5f2b8c48ecf730ff7979ba5709ae16bf594cd6c
Mahmoud Rashed <37276859+MahmoudRashed2505@users.noreply.github.com>
2020-07-05 18:02:00 +0200

    Added URL Shortener Module

---
+++ shorten.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://tinyurl.com/create.php?url="
+long_url = input("Enter long url: ")
+response = requests.get(url+long_url)
+
+soup = BeautifulSoup(response.content,'html.parser')
+
+link = soup.find_all('b')
+print("Original url: {}\tShorten url: {}".format(long_url,str(link[1])[3:-4]))
