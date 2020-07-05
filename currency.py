538cb16f6795f50d9146390c98ec3500525b72f3
Spartan <spartan@pop-os.localdomain>
2020-07-05 05:08:38 +0200

    Finishing Currency Converter

--- currency.py
+++ currency.py
@@ -3,13 +3,12 @@ import json
 import requests
 import csv
 from os import system as sys
+
 token = "&compact=ultra&apiKey=52456c84f29257e83786"
 url = "https://free.currconv.com/api/v7/convert?q="
-#headers =
 def convert(baseCurrency,convertedCurrency,amount):
     sys("clear")
     api_url = url+baseCurrency+'_'+convertedCurrency+token
-    print(api_url)
     response = requests.get(api_url)
     convert_rate = json.loads(response.content.decode('utf-8'))
     rate = 0
@@ -17,12 +16,26 @@ def convert(baseCurrency,convertedCurrency,amount):
         rate = value
     return float(amount)*float(rate)
 
-def get_currencies():
-
+def choose_currency():
+    sys("clear")
+    alphacode = []
+    print("Please Choose the Base and Converted Currencies:\n")
+    with open("codes-all_csv.csv",mode='r') as csv_file:
+          csv_reader = csv.DictReader(csv_file)
+          line_count = 0
+          for row in csv_reader:
+              if line_count==0:
+                 print("Country Name:\t\t\tCurrency:\n")
+                 line_count += 1
+              print("{}- {}----> {}".format(line_count,row['Entity'],row["AlphabeticCode"]))
+              alphacode.append(row["AlphabeticCode"])
+              line_count += 1
+    baseCurrency = input("\n\nEnter the Base Currency: ")
+    convertedCurrency = input("Which Currency do you want to convert to: ")
+    return baseCurrency,convertedCurrency
 
 sys("clear")
-get_currencies()
-#amountToBeConverted = input("Enter Amount: ")
-#baseCurrency,convertedCurrency = get_currencies()
-#convertedAmount=convert(baseCurrency,convertedCurrency,amountToBeConverted)
-#print("{} From {} To {} = {}\n".format(amountToBeConverted,baseCurrency,convertedCurrency,convertedAmount))
+amountToBeConverted = input("Enter Amount: ")
+baseCurrency,convertedCurrency = choose_currency()
+convertedAmount=convert(baseCurrency,convertedCurrency,amountToBeConverted)
+print("{} {} = {:.3f} {} \n".format(amountToBeConverted,baseCurrency,convertedAmount,convertedCurrency))
